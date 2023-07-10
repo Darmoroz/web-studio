11d90f2ff7d6e8af54740712ad0e514eaa0b3862
Olexandr Darmoroz <darmoroz99@gmail.com>
2023-07-10 14:45:06 +0300

    'add close modal'

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,29 @@
-(() => {
-	const refs = {
-		openModalBtn: document.querySelector("[data-modal-open]"),
-		closeModalBtn: document.querySelector("[data-modal-close]"),
-		modal: document.querySelector("[data-modal]"),
-	};
+const refs = {
+  openModalBtn: document.querySelector('[data-modal-open]'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  modal: document.querySelector('[data-modal]'),
+};
 
-	refs.openModalBtn.addEventListener("click", toggleModal);
-	refs.closeModalBtn.addEventListener("click", toggleModal);
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
+refs.modal.addEventListener('click', e => {
+  if (e.target === refs.modal) {
+    document.body.classList.remove('modal-open');
+    refs.modal.classList.add('is-hidden');
+  }
+});
 
-	function toggleModal() {
-		document.body.classList.toggle("modal-open");
-		refs.modal.classList.toggle("is-hidden");
-	}
-})();
+window.addEventListener('keydown', e => {
+  if (e.code !== 'Escape') {
+    return;
+  }
+  if (document.body.classList.contains('modal-open')) {
+    document.body.classList.remove('modal-open');
+    refs.modal.classList.add('is-hidden');
+  }
+});
+
+function toggleModal() {
+  document.body.classList.toggle('modal-open');
+  refs.modal.classList.toggle('is-hidden');
+}
