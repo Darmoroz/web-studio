77ae5b97bf89457b2e2d13026527f73ff07ce21d
Olexandr Darmoroz <darmoroz99@gmail.com>
2023-07-10 17:42:17 +0300

    'add overflow to body when modal open'

--- js/modal.js
+++ js/modal.js
@@ -6,25 +6,32 @@ const refs = {
 
 refs.openModalBtn.addEventListener('click', toggleModal);
 refs.closeModalBtn.addEventListener('click', toggleModal);
+
 refs.modal.addEventListener('click', e => {
   if (e.target === refs.modal) {
     document.body.classList.remove('modal-open');
     refs.modal.classList.add('is-hidden');
+    document.body.style.overflow = 'auto';
   }
 });
 
 window.addEventListener('keydown', e => {
   if (e.code !== 'Escape') {
-    console.log('abra');
     return;
   }
   if (document.body.classList.contains('modal-open')) {
     document.body.classList.remove('modal-open');
     refs.modal.classList.add('is-hidden');
+    document.body.style.overflow = 'auto';
   }
 });
 
 function toggleModal() {
+  if (document.body.style.overflow === 'hidden') {
+    document.body.style.overflow = 'auto';
+  } else {
+    document.body.style.overflow = 'hidden';
+  }
   document.body.classList.toggle('modal-open');
   refs.modal.classList.toggle('is-hidden');
 }
